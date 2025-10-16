4994cb5452f853758b909b06b4d69570a17189cc
Yuki Kawai <59817383+ajinori-256@users.noreply.github.com>
2025-10-16 19:27:19 +0900

    feat: add Instagram and menu icons, enhance header layout and styles (#16)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,39 @@
+"use client";
+import react, { useState } from "react";
 import Link from "next/link";
 import styles from "./style.module.css";
+import Image from "next/image";
 
 export default function Header() {
+  const [isOpen, setOpen] = useState(false);
+  const open = () => {
+    setOpen((isOpen) => !isOpen);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.inner}>
-        <Link href="/" className={styles.logo}>
-          市大祭
-        </Link>
-        <nav className={styles.nav}>
+        <div className={styles.headerLogo}>
+          <Image
+            src="/images/logo.webp"
+            alt="ロゴ"
+            width={512}
+            height={512}
+            className={styles.logo}
+          />
+        </div>
+
+        <button
+          className={styles.menuButton}
+          onClick={open}
+          aria-label="メニュー"
+        >
+          <Image src="/images/menu.svg" alt="メニュー" width={32} height={32} />
+        </button>
+        <nav className={styles.headerLinks}>
+          <Link href="/" className={styles.link}>
+            ホーム
+          </Link>
           <Link href="/projects" className={styles.link}>
             企画検索
           </Link>
@@ -21,6 +46,20 @@ export default function Header() {
           <Link href="/access" className={styles.link}>
             アクセス
           </Link>
+          <a
+            href="https://www.instagram.com/ichidai_sai/"
+            className={styles.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
+            <Image
+              src="/images/instagram.svg"
+              alt="Instagram"
+              width={32}
+              height={32}
+            />
+          </a>
         </nav>
       </div>
     </header>
