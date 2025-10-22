274b4f1d5b8d30ab078a4db99ca6e5522375ac21
hina81 <i20203@e.hiroshima-cu.ac.jp>
2025-10-22 16:37:47 +0900

    いらないのけす

--- src/app/projects/[category]/page.tsx
+++ src/app/projects/[category]/page.tsx
@@ -1,25 +1,11 @@
 import { getStoreData } from "@/libs/dummyData";
 import StoreListPage from "@/components/StoreListPage";
-import { notFound } from "next/navigation";
 
-const categoryMap = {
-  food: "飲食",
-  shop: "物販・展示",
-} as const;
-
-type Props = {
-  params: { category: string };
-};
-
-export default function Page({ params }: Props) {
-  const { category } = params;
-  const categoryName = categoryMap[category as keyof typeof categoryMap];
-  if (!categoryName) notFound();
-
-  const stores = getStoreData().filter((s) => s.category === categoryName);
+export default function Page() {
+  const stores = getStoreData();
   return (
     <StoreListPage
-      heading={`${categoryName}の出店一覧`}
+      heading="出店一覧"
       headingEnglish="List of Stalls"
       stores={stores}
     />
