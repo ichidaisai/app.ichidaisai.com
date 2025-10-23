7117847d879381596ff9cb9c74e8d44d18e77251
kaihiroi1 <217571199+kaihiroi1@users.noreply.github.com>
2025-10-24 03:15:20 +0900

    コスプレページ作成 (#31)
    * コスプレページ作成
    
    * アクセス修正 (#35)
    
    * 見出し用コンポーネント
    
    * 色々修正
    
    * マップをイベントに
    
    * ロゴ押したらホーム&可変 (#34)
    
    * ロゴのサイズをremからvwに変 更
    
    * ロゴサイズ可変，スポンサーのマージン拡大，ロゴ押したらホーム
    
    * fix: ロゴのheightを固定値にする
    
    * fix: 出店一覧の上のpaddingを狭めた
    
    ---------
    
    Co-authored-by: PoPodada <goito.722@gmail.com>
    
    * noimage追加 (#38)
    
    * お知らせを追加 (#40)
    
    * お知らせ修正
    
    * スポンサー直す
    
    * これも
    
    * リファクタリング (#42)
    
    * fix: ディレクトリ構成をリプレイス
    
    * fix: biomeがbiomeじゃなかったので修正
    
    * fix: format
    
    * fix: アクセス、ゲスト、企画検索ページをリファクタリング
    
    * fix: format
    
    * Update header links for clarity and consistency (#41)
    
    * Update header links for clarity and consistency
    
    * お知らせを追加 (#40)
    
    * お知らせ修正
    
    * スポンサー直す
    
    * これも
    
    * リファクタリング (#42)
    
    * fix: ディレクトリ構成をリプレイス
    
    * fix: biomeがbiomeじゃなかったので修正
    
    * fix: format
    
    * fix: アクセス、ゲスト、企画検索ページをリファクタリング
    
    * fix: format
    
    * Update header links for clarity and consistency
    
    ---------
    
    Co-authored-by: hina81 <144189396+hina81@users.noreply.github.com>
    Co-authored-by: Go ito <105594727+PoPodada@users.noreply.github.com>
    Co-authored-by: hina81 <i20203@e.hiroshima-cu.ac.jp>
    
    * コスプレページ作成
    
    * 見出し揃える
    
    * これいらない
    
    ---------
    
    Co-authored-by: hina81 <144189396+hina81@users.noreply.github.com>
    Co-authored-by: Gutchii <175215582+Gutchii@users.noreply.github.com>
    Co-authored-by: PoPodada <goito.722@gmail.com>
    Co-authored-by: Go ito <105594727+PoPodada@users.noreply.github.com>
    Co-authored-by: Yuki Kawai <59817383+ajinori-256@users.noreply.github.com>
    Co-authored-by: hina81 <i20203@e.hiroshima-cu.ac.jp>

---
+++ src/app/(standard)/cosplay/page.tsx
@@ -0,0 +1,48 @@
+import Title from "@/components/Title";
+import styles from "./style.module.css";
+export default function CosplayContest() {
+  return (
+    <>
+      <Title heading="コスプレイベント" headingEnglish="Cosplay Event" />
+      <div>
+        <div className={styles.imageRow}>
+          <img
+            src="/projects/cosplay_contest1.png"
+            className={styles.sideImage}
+          />
+          <img
+            src="/projects/cosplay_contest2.png"
+            className={styles.mainImage}
+          />
+          <img
+            src="/projects/cosplay_contest3.png"
+            className={styles.sideImage}
+          />
+        </div>
+        <div className={styles.textbox}>
+          <p>
+            コスプレコンテスト変身の魔法、開幕！コスプレで新しい自分に大変身！
+            <br />
+            大胆に、豪華に、ユーモラスに個性あふれる参加者たちがステージを彩ります！
+            <br />
+            勝敗を決めるのは審査員ではなく、
+            <span className={styles.highlight}>会場のあなたの一票！</span>
+            <br />
+            参加者の魅力を見逃さず、ぜひ投票で盛り上げてください！
+            <br />
+            優勝者には<span className={styles.highlight}>豪華景品</span>
+            をご用意！見るだけでも楽しめる、
+            <br />
+            参加すればもっと楽しい、この瞬間を一緒に作り上げましょう！
+            <br />
+          </p>
+        </div>
+        <div className={styles.form_link}>
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLScOvKlPFdLcEpKyfXEOcwVFdcpuWxW8Tudtq020oLcUMHGQBg/viewform?usp=header">
+            参加したい人はこちらをクリック
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
