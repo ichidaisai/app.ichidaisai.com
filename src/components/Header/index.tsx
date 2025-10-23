88f46bb78ef1fbc553c72b1dc4fe739645e789ac
Yuki Kawai <59817383+ajinori-256@users.noreply.github.com>
2025-10-24 02:44:49 +0900

    Update header links for clarity and consistency (#41)
    * Update header links for clarity and consistency
    
    * お知らせを追加 (#40)
    
    * お知らせ修正
    
    * スポンサー直す
    
    * これも
    
    * リファクタリング (#42)
    
    * fix: ディレクトリ構成をリプレイス
    
    * fix: biomeがbiomeじゃなかったので修正
    
    * fix: format
    
    * fix: アクセス、ゲスト、企画検索ページをリファクタリング
    
    * fix: format
    
    * Update header links for clarity and consistency
    
    ---------
    
    Co-authored-by: hina81 <144189396+hina81@users.noreply.github.com>
    Co-authored-by: Go ito <105594727+PoPodada@users.noreply.github.com>
    Co-authored-by: hina81 <i20203@e.hiroshima-cu.ac.jp>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,61 +5,61 @@ import { useState } from "react";
 import styles from "./style.module.css";
 
 export default function Header() {
-	const [isOpen, setOpen] = useState(false);
-	const open = () => {
-		setOpen((isOpen) => !isOpen);
-	};
+  const [isOpen, setOpen] = useState(false);
+  const open = () => {
+    setOpen((isOpen) => !isOpen);
+  };
 
-	return (
-		<header className={styles.header}>
-			<div className={styles.inner}>
-				<div className={styles.headerLogo}>
-					<Link href="/">
-						<Image
-							src="/images/logo.webp"
-							alt="ロゴ"
-							width={512}
-							height={512}
-							className={styles.logo}
-						/>
-					</Link>
-				</div>
+  return (
+    <header className={styles.header}>
+      <div className={styles.inner}>
+        <div className={styles.headerLogo}>
+          <Link href="/">
+            <Image
+              src="/images/logo.webp"
+              alt="ロゴ"
+              width={512}
+              height={512}
+              className={styles.logo}
+            />
+          </Link>
+        </div>
 
-				<button className={styles.menuButton} onClick={open} type="button">
-					<Image src="/images/menu.svg" alt="メニュー" width={32} height={32} />
-				</button>
-				<nav className={styles.headerLinks}>
-					<Link href="/" className={styles.link}>
-						ホーム
-					</Link>
-					<Link href="/projects" className={styles.link}>
-						企画検索
-					</Link>
-					<Link href="/guest" className={styles.link}>
-						ゲスト
-					</Link>
-					<Link href="/event" className={styles.link}>
-						イベント
-					</Link>
-					<Link href="/access" className={styles.link}>
-						アクセス
-					</Link>
-					<a
-						href="https://www.instagram.com/ichidai_sai/"
-						className={styles.link}
-						target="_blank"
-						rel="noopener noreferrer"
-						aria-label="Instagram"
-					>
-						<Image
-							src="/images/instagram.svg"
-							alt="Instagram"
-							width={32}
-							height={32}
-						/>
-					</a>
-				</nav>
-			</div>
-		</header>
-	);
+        <button className={styles.menuButton} onClick={open} type="button">
+          <Image src="/images/menu.svg" alt="メニュー" width={32} height={32} />
+        </button>
+        <nav className={styles.headerLinks}>
+          <Link href="/" className={styles.link}>
+            ホーム
+          </Link>
+          <Link href="/projects" className={styles.link}>
+            出展一覧
+          </Link>
+          <Link href="/guest" className={styles.link}>
+            ゲスト
+          </Link>
+          <Link href="/timetable" className={styles.link}>
+            タイムテーブル
+          </Link>
+          <Link href="/access" className={styles.link}>
+            アクセス
+          </Link>
+          <a
+            href="https://www.instagram.com/ichidai_sai/"
+            className={styles.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
+          >
+            <Image
+              src="/images/instagram.svg"
+              alt="Instagram"
+              width={32}
+              height={32}
+            />
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
 }
