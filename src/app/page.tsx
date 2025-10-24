9cf0fc588bed9c52ded62e7a30a40953e29efc2b
akinori1006 <217571102+akinori1006@users.noreply.github.com>
2025-10-24 10:34:19 +0900

    注意事項とごみの分別の項目を追加しました

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,6 +39,84 @@ export default function Page() {
           </p>
         </div>
       </div>
+
+      <div className={styles.container}>
+        <div className={styles.textWrapper}>
+          <center>
+            <h1 className={styles.title}>
+              注意事項
+            </h1>
+            <h2 className={styles.subtitle}>
+              <b>本日はご来場ありがとうございます</b>
+            </h2>
+          <p className={styles.notice_text}>
+            全てのご来場の方が安心して<span className={styles.br}></span>市大祭をお楽しみいただけるよう
+            <br />
+            以下の点にご協力をお願いします。
+          </p>
+          </center>
+          <div className={styles.notice}>
+            <div>
+              <center>
+              <h3>全面禁煙</h3>
+              <Image
+                src="/images/kinen_mark.png"
+                className={styles.notice_Image}
+                alt="全面禁煙"
+                width={300}
+                height={300}
+              />
+              </center>
+            </div>
+            <div>
+              <center>
+              <h3>ペット禁止</h3>
+              <Image
+                src="/images/pettokinsi_mark.png"
+                className={styles.notice_Image}
+                alt="ペット禁止"
+                width={300}
+                height={300}
+              />
+              </center>
+            </div>
+            <div>
+              <center>
+              <h3>
+                全面飲酒禁止
+                <br />
+                酒類の持ち込み不可
+              </h3>
+              <Image
+                src="/images/osakekinsi_mark.png"
+                className={styles.notice_Image}
+                alt="全面飲酒禁止酒類の持ち込み不可"
+                width={300}
+                height={300}
+              />
+              </center>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.textWrapper}>
+          <center>
+            <b>
+              <h2 className={styles.subtitle}>
+                ごみの分別にご協力をお願いします！
+              </h2>
+            </b>
+            <Image
+              src="/images/gomibunbetu.png"
+              alt="ごみの分別"
+              width={300}
+              height={300}
+              className={styles.notice_Image}
+            />
+          </center>
+        </div>
+      </div>
     </>
   );
 }
