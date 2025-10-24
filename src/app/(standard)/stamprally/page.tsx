00a604dafa8427be7fae80dc54418021bf3864da
Gutchii <175215582+Gutchii@users.noreply.github.com>
2025-10-24 21:48:32 +0900

    stamprallyページが大方完成

--- src/app/(standard)/stamprally/page.tsx
+++ src/app/(standard)/stamprally/page.tsx
@@ -1,5 +1,6 @@
 import Title from "@/components/Title";
 import styles from "./style.module.css";
+import { Kaisei_Opti } from "next/font/google";
 export default function Page() {
   return (
     <>
@@ -9,19 +10,63 @@ export default function Page() {
       </div> */}
       <Title heading="スタンプラリー" headingEnglish="Stamp Rally" />
 
+      <div className={styles.subtitle}>
+          <p>
+            市大祭でスタンプラリーを開催！！！<br />
+            ゴールした方には豪華景品が！台紙はパンフレットの裏をご覧ください！！
+          </p>
+      </div>
+
       {/* コンテンツ部分 */}
-      <div className={styles.content_wrapper}>
-        {/* 左側：本文 */}
-        <div className={styles.wrapper}>
-          {/* 右側：画像 */}
-          <div className={styles.image_section}>
-            <img src="/images/bingo_img.png" className={styles.image} />
-          </div>  
-          <div className={styles.text_box}>
-            <p>
-                市大祭スタンプラリーへようこそ！
-            </p>
+      <div className={styles.line}>
+        <div className={styles.content_wrapper}>
+          <div className={styles.minititle}>
+            <b>①パンフレットの裏を確認しよう！</b>
           </div>
+          <div className={styles.wrapper}>
+            {/* 右側：画像 */}
+            <div className={styles.image_section}>
+              <img src="/images/stamprally_tori.png" className={styles.image} />
+            </div>  
+            <div className={styles.text_box}>
+              <p>
+                まずはパンフレットの裏を確認しよう！
+                次のステップに進むためのヒントが見つかるかも！
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.minititle}>
+            <b>②目的地へレッツゴー！</b>
+          </div>
+          <div className={styles.wrapper}>
+            {/* 右側：画像 */}
+            <div className={styles.image_section}>
+              <img src="/images/stamposu.png" className={styles.image} />
+            </div>  
+            <div className={styles.text_box}>
+              <p>
+                パンフレットを頼りにスタート！各目的地でスタンプをゲット！
+              </p>
+            </div>
+          </div>
+
+          <div className={styles.minititle}>
+            <b>③ヒントを頼りに次の目的地にGO！</b>
+          </div>
+          <div className={styles.wrapper}>
+            {/* 右側：画像 */}
+            <div className={styles.image_section}>
+              <img src="/images/stampcomplete.png" className={styles.image} />
+            </div>  
+            <div className={styles.text_box}>
+              <p>
+                現在地にあるヒントから次の目的地を探そう！
+                全部集めたら玄関受付でいいことが待っているかも！？
+              </p>
+            </div>
+          </div>
+          
         </div>
       </div>
     </>
